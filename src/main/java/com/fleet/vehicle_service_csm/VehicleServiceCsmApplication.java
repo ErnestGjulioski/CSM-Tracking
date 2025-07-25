aee2490e85b674dc5a9203a0baf0eaccc15015f1
Ernest Gjulioski <gjulioski.ernest@gmail.com>
2025-07-25 16:38:23 +0200

    add Spring structure

---
+++ src/main/java/com/fleet/vehicle_service_csm/VehicleServiceCsmApplication.java
@@ -0,0 +1,13 @@
+package com.fleet.vehicle_service_csm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceCsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceCsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleet/vehicle_service_csm/VehicleServiceCsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleet.vehicle_service_csm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceCsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
